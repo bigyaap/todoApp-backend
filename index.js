0182543dcff9b2b013e44fe0a6bdd18fb6893327
bigyaap <pokhrel.bigyaa2000@gmail.com>
2023-09-29 15:02:47 +0545

    / route

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = require('./routes')
+// const express = require('express')
+// const router = require('./routes')
 
-const app = express()
-app.use(express.json())
-app.use(router)
+// const app = express()
+// app.use(express.json())
+// app.use(router)
 
-app.get('/', (req, res) => {
-    res.json({ message: 'I am alive!' });
-});
+// app.get('/', (req, res) => {
+//     res.json({ message: 'I am alive!' });
+// });
 
-app.listen(8080, () => console.log('Server listening on port 8080'))
+// app.listen(8080, () => console.log('Server listening on port 8080'))
